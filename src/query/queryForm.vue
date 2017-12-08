4f5f9b9065ad39fa29dc00f9ec6238e034e80311
HoseaLee <hosealee0208@gmail.com>
2017-12-08 14:58:51 +0800

    Add show more btn and show-line prop

--- src/query/queryForm.vue
+++ src/query/queryForm.vue
@@ -15,7 +15,7 @@
     </template>
     <Form :label-width="80">
       <template v-for="(tags, index) in formLayout">
-        <Row type="flex" v-show="((index+1)>showLine?isShow:true)">
+        <Row type="flex" v-show="((index+1)>showLineNum?isShow:true)">
           <template v-for="(tag, _index) in tags">
             <Col span="8">
             <FormItem :label="getLabel(tag)">
@@ -24,7 +24,7 @@
             </Col>
           </template>
         </Row>
-        <div class="line" v-show="((index+1)>showLine?isShow:true)"></div>
+        <div class="line" v-show="((index+1)>showLineNum?isShow:true)"></div>
       </template>
       <Row type="flex" justify="center">
                 <span @click="showHideSwitch" class="showMoreBtn">
@@ -66,15 +66,16 @@
     /*left: 0;*/
     box-sizing: border-box;
   }
-  span.showMoreBtn{
+
+  span.showMoreBtn {
     border-left: 2px solid #eee;
     border-right: 2px solid #eee;
     border-bottom: 2px solid #eee;
-    padding:3px;
+    padding: 3px;
     -moz-border-radius-bottomleft: 5px;
     -moz-border-radius-bottomright: 5px;
-    cursor:pointer;
-    margin:0 auto;
+    cursor: pointer;
+    margin: 0 auto;
   }
 </style>
 <script>
@@ -89,7 +90,7 @@
   import QueryCheckbox from "./queryCheckbox.vue"
 
   export default {
-    props: ["fields", "layout", "value", "buttons", "changed", "submit"],
+    props: ["fields", "layout", "value", "buttons", "changed", "submit", "show-line"],
     components: {
       "str": QueryString,
       "iselect": QuerySelect,
@@ -119,16 +120,20 @@
       //selected tag { name: "", label: "", val: ""}
       let selected = [];
       //showLine
-      let showLine = 2,
+      let showLineNum = (typeof this.showLine == "number") ?
+          this.showLine //showLine is number, adopt showLine
+          : ((typeof this.showLine == "boolean") //showLine isn't number and check it if boolean
+          ? (this.showLine ? 2 : formLayout.length + 1) //showLine is a boolean. if it's true, set it to 2, if not, set it equal to formLayout's length + 1
+          : 2),//showLine is not a boolean yet, set default value to 2
         isShow = false;
-      console.log("showLine=", showLine, typeof showLine);
+      console.log("showLine=", showLineNum, typeof showLineNum);
       return {
         store,
         formLayout,
         btnOpt,
         btnJustify,
         selected,
-        showLine,
+        showLineNum,
         isShow
       }
     },
@@ -172,21 +177,29 @@
                       ((field['choices'][choice]['value'] == val[v]) && vals.push(field['choices'][choice]['label']));
                     }
                   }
-                  this.selected.push({name: field['name'], label: field['label'], val: vals.toString()});
+                  this.selected.push({
+                    name: field['name'],
+                    label: field['label'],
+                    val: vals.toString()
+                  });
                 } else {
-                  if((typeof val == "string") && val){
+                  if ((typeof val == "string") && val) {
                     for (let choice in field['choices']) {
                       (field['choices'][choice]['value'] == val) && vals.push(field['choices'][choice]['label'])
                     }
-                    this.selected.push({name: field['name'], label: field['label'], val: vals.toString()});
+                    this.selected.push({
+                      name: field['name'],
+                      label: field['label'],
+                      val: vals.toString()
+                    });
                   }
 
                 }
                 break;
               case "date":
-                if(typeof val == "string")val = new Date(val);
-                let format = field.hasOwnProperty("format")?field['format']:"yyyy-MM-dd",
-                  formattedVal = format.replace("yyyy", val.getFullYear()).replace("MM", ((val.getMonth()+1)<10)?"0"+(val.getMonth()+1):(val.getMonth()+1)).replace("dd", ((val.getDate())<10)?"0"+(val.getDate()):(val.getDate()))
+                if (typeof val == "string")val = new Date(val);
+                let format = field.hasOwnProperty("format") ? field['format'] : "yyyy-MM-dd",
+                  formattedVal = format.replace("yyyy", val.getFullYear()).replace("MM", ((val.getMonth() + 1) < 10) ? "0" + (val.getMonth() + 1) : (val.getMonth() + 1)).replace("dd", ((val.getDate()) < 10) ? "0" + (val.getDate()) : (val.getDate()))
                 this.selected.push({name: field['name'], label: field['label'], val: formattedVal});
                 break;
               case "radio":
@@ -242,7 +255,7 @@
 //                        }
 //                        this.createSelectedTag();
         let re = this.submit(this.store.getVal());
-        if(re){
+        if (re) {
           this.selected = [];
           this.createSelectedTag();
         }
