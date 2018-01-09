f25e9ca969ffc062c8a3f01108afb18bf57398df
HoseaLee <hosealee0208@gmail.com>
2018-01-09 14:47:47 +0800

    no message

--- src/uploader/Uploader.vue
+++ src/uploader/Uploader.vue
@@ -1,5 +1,7 @@
 <template>
     <div :id="dynamicID">
+        <a class="uploader-picker btn btn-primary btn-o"></a>
+        <span id="urlBox"></span>
         <div class="uploader-list"></div>
     </div>
 </template>
@@ -8,7 +10,7 @@
         props: ["config"],
         data: function () {
             var dynamicID = "uploader_" + (new Date()).getTime(),
-                    configuration = Object.assign({domId: dynamicID}, this.config),
+                    configuration = Object.assign({domId: "#"+dynamicID}, this.config),
                     uploader = null;
             return {
                 dynamicID,
